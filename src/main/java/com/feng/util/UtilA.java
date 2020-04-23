1d137aaf4f8d5a1ff80c74d0c1dd56ff2260382e
panyangfeng <15797631175@163.com>
2020-04-23 14:59:05 +0800

    1

---
+++ src/main/java/com/feng/util/UtilA.java
@@ -0,0 +1,20 @@
+package com.feng.util;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author pyf 2020-04-23
+ */
+public class UtilA
+{
+    private String name;
+
+    private String age;
+
+    public static void main(String[] args)
+    {
+        System.out.println("hello world!");
+    }
+}
