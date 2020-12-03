7ca9b4a7aaee72de5860255ca281fa0a56fda1c8
panyangfeng <15797631175@163.com>
2020-12-03 11:31:48 +0800

    print hello world branch-01

--- src/main/java/com/feng/GitDemoApplication.java
+++ src/main/java/com/feng/GitDemoApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitDemoApplication
 {
-
     public static void main(String[] args)
     {
+        System.out.println("hello world branch-01");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
