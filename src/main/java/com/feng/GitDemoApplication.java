4b1df0735de8a517b0ed32568ce3eb5e5d0dcff1
panyangfeng <15797631175@163.com>
2020-12-03 11:13:12 +0800

    print hello world cherry-pick

--- src/main/java/com/feng/GitDemoApplication.java
+++ src/main/java/com/feng/GitDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitDemoApplication
 {
+
     public static void main(String[] args)
     {
         System.out.println("hello world branch-01");
